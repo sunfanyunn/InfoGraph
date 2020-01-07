701382662f79dde89990ed679157ef38e3216df9
fanyun sun <sunfanyun@gmail.com>
2020-01-07 22:14:34 +0800

    Update deepinfomax.py

--- unsupervised/deepinfomax.py
+++ unsupervised/deepinfomax.py
@@ -43,8 +43,6 @@ class GcnInfomax(nn.Module):
 
     self.init_emb()
 
-    self.adj_loss = True
-
   def init_emb(self):
     initrange = -1.5 / self.embedding_dim
     for m in self.modules():
@@ -67,12 +65,8 @@ class GcnInfomax(nn.Module):
 
     mode='fd'
     measure='JSD'
-
-    if args.local:
-        local_global_loss = local_global_loss_(l_enc, g_enc, edge_index, batch, measure)
-    else:
-        local_global_loss = 0
-
+    local_global_loss = local_global_loss_(l_enc, g_enc, edge_index, batch, measure)
+ 
     if self.prior:
         prior = torch.rand_like(y)
         term_a = torch.log(self.prior_d(prior)).mean()
@@ -80,11 +74,6 @@ class GcnInfomax(nn.Module):
         PRIOR = - (term_a + term_b) * self.gamma
     else:
         PRIOR = 0
-    
-    if self.adj_loss:
-        adj_loss = adj_loss_(l_enc, g_enc, edge_index, batch)
-        print(local_global_loss, PRIOR, adj_loss)
-        return local_global_loss + PRIOR + adj_loss
     
     return local_global_loss + PRIOR
 
@@ -151,7 +140,7 @@ if __name__ == '__main__':
             accuracies['randomforest'].append(res[3])
             print(accuracies)
 
-    tpe  = ('local' if args.local else '') + ('global' if args.glob else '') + ('prior' if args.prior else '')
+    tpe  = ('local' if args.local else '') + ('prior' if args.prior else '')
     with open('new_log', 'a+') as f:
         s = json.dumps(accuracies)
         f.write('{},{},{},{},{},{},{}\n'.format(args.DS, tpe, args.num_gc_layers, epochs, log_interval, lr, s))
